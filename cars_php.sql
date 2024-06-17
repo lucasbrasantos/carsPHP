7b8c70150340ceeac1bd519f0307b93b7a02fa00
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-06-16 21:35:31 -0300

    update sql files

--- cars_php.sql
+++ cars_php.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/05/2024 às 02:00
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 17/06/2024 às 02:35
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,21 +32,25 @@ CREATE TABLE `cars` (
   `marca` varchar(128) NOT NULL,
   `cor` varchar(128) NOT NULL,
   `ano` int(11) NOT NULL,
-  `preco` int(11) NOT NULL
+  `preco` int(11) NOT NULL,
+  `usado` varchar(3) NOT NULL,
+  `adicionais` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`adicionais`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `cars`
 --
 
-INSERT INTO `cars` (`id`, `marca`, `cor`, `ano`, `preco`) VALUES
-(29, 'Toyota', 'Blue', 1933, 60867),
-(30, 'Mercedes-Benz', 'White', 2045, 74016),
-(31, 'Mercedes-Benz', 'Blue', 1996, 36625),
-(32, 'Honda', 'Blue', 1976, 82839),
-(33, 'Toyota', 'Red', 2067, 83159),
-(34, 'Toyota', 'Red', 1936, 64803),
-(35, 'Ford', 'Red', 2007, 34922);
+INSERT INTO `cars` (`id`, `marca`, `cor`, `ano`, `preco`, `usado`, `adicionais`) VALUES
+(55, 'Ford', 'Green', 2052, 76634, 'não', '[\"Airbag\"]'),
+(56, 'Ford', 'White', 1902, 37728, 'sim', '[\"Estofado de couro\",\"GPS\"]'),
+(65, 'Ford', 'Black', 2048, 77280, 'sim', '[\"Vidro com insulfilm\",\"Airbag\"]'),
+(66, 'Ford', 'Silver', 2086, 1073, 'sim', '[\"Possui tela touch\",\"GPS\"]'),
+(67, 'Mercedes-Benz', 'Blue', 1975, 28567, 'sim', '[]'),
+(70, 'Ford', 'Silver', 1993, 62668, 'não', '[\"Possui tela touch\",\"Airbag\"]'),
+(71, 'Mercedes-Benz', 'White', 1940, 49758, 'sim', '[\"GPS\"]'),
+(72, 'BMW', 'White', 2049, 7119, 'não', '[\"Vidro com insulfilm\",\"Pe\\u00e7as originais\",\"Possui tela touch\",\"Estofado de couro\"]'),
+(73, 'Ford', 'White', 1904, 16039, 'não', '[\"Vidro com insulfilm\",\"Pe\\u00e7as originais\",\"Estofado de couro\",\"GPS\"]');
 
 --
 -- Índices para tabelas despejadas
@@ -66,7 +70,7 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT de tabela `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database.sql
+++ database.sql
@@ -1,6 +1,10 @@
+-- Criação do banco de dados
 CREATE DATABASE `cars_php` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 
+-- Seleção do banco de dados
+USE `cars_php`;
 
+-- Criação da tabela `cars`
 CREATE TABLE `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `marca` varchar(128) NOT NULL,
@@ -9,3 +13,8 @@ CREATE TABLE `cars` (
   `preco` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Alteração da tabela `cars` para adicionar novos campos
+ALTER TABLE `cars`
+ADD COLUMN `usado` VARCHAR(3) NOT NULL AFTER `preco`,
+ADD COLUMN `adicionais` JSON AFTER `usado`;
